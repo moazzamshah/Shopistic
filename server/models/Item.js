2d5e2e843094496da350851f1e2efabfb4d049f9
Ahmad575-a <ahmad.alghetheth@gmail.com>
2021-06-15 11:56:04 +0200

    created the products schema

---
+++ server/models/Item.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ItemSchema = new Schema({
+    sellerId: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+    },
+    user: {
+        type: String
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: false
+    },
+    picture: {
+        type: String,
+        required: false
+    },
+    category: {
+        type: String,
+        required: false
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = Item = mongoose.model('items', ItemSchema);
