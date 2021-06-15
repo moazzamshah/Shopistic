1811fc4d8757526fbde7b04ceb1ce76e044da3e6
Ahmad575-a <ahmad.alghetheth@gmail.com>
2021-06-15 14:50:43 +0200

    created Cart_item schema

---
+++ server/models/Cart_Item.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CartItemSchema = new Schema({
+	userId: {
+		type: Schema.Types.ObjectId,
+		ref: 'users'
+	},
+	itemId: {
+		type: Schema.Types.ObjectId,
+		ref: 'items'
+	},
+	date: {
+		type: Date,
+		default: Date.now
+	}
+});
+
+module.exports = mongoose.model('cartItem', CartItemSchema);
