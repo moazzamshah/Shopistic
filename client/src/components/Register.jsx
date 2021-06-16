d489fbf9075f584b37b81a96504001485583eabf
ArslanRama <arslanrama84@gmail.com>
2021-06-16 11:57:43 +0200

    forms impplemented with validation

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,179 +1,135 @@
-import React, { useState, useRef } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React from 'react'
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from 'yup';
 
-import Form from "react-validation/build/form";
-import Input from "react-validation/build/input";
-import CheckButton from "react-validation/build/button";
-import { isEmail } from "validator";
+export const Register = () => {
+
+    const validationSchema = Yup.object().shape({
+        fullname: Yup.string().required('Fullname is required'),
+        username: Yup.string()
+            .required('Username is required')
+            .min(6, 'Username must be at least 6 characters')
+            .max(20, 'Username must not exceed 20 characters'),
+        email: Yup.string()
+            .required('Email is required')
+            .email('Email is invalid'),
+        password: Yup.string()
+            .required('Password is required')
+            .min(6, 'Password must be at least 6 characters')
+            .max(40, 'Password must not exceed 40 characters'),
+        confirmPassword: Yup.string()
+            .required('Confirm Password is required')
+            .oneOf([Yup.ref('password'), null], 'Confirm Password does not match'),
+        acceptTerms: Yup.bool().oneOf([true], 'Accept Terms is required')
+    });
 
 
-import { register } from "../actions/auth";
+    /* 
+    The useForm() hook function returns an object that we use following methods:
+    register: register inputs
+    handleSubmit: handle form submit
+    reset:reset the form
+    */
+     // functions to build form returned by useForm() hook
+    const { register, handleSubmit, reset, formState: { errors } } = useForm({
+        resolver: yupResolver(validationSchema)
+    });
 
-const required = (value) => {
-  if (!value) {
-    return (
-      <div className="alert alert-danger" role="alert">
-        This field is required!
-      </div>
-    );
-  }
-};
-
-// Valid email: required, email format
-const validEmail = (value) => {
-  if (!isEmail(value)) {
-    return (
-      <div className="alert alert-danger" role="alert">
-        This is not a valid email.
-      </div>
-    );
-  }
-};
-
-// Valid username: required, between 3 and 20 characters
-const vusername = (value) => {
-  if (value.length < 3 || value.length > 20) {
-    return (
-      <div className="alert alert-danger" role="alert">
-        The username must be between 3 and 20 characters.
-      </div>
-    );
-  }
-};
-
-// Valid password: required, between 6 and 40 characters
-const vpassword = (value) => {
-  if (value.length < 6 || value.length > 40) {
-    return (
-      <div className="alert alert-danger" role="alert">
-        The password must be between 6 and 40 characters.
-      </div>
-    );
-  }
-};
-
-// Dispatching register action and show response message (successful or error).
-const Register = () => {
-  const form = useRef();
-  const checkBtn = useRef();
-
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [successful, setSuccessful] = useState(false);
-
-  const { message } = useSelector(state => state.message);
-  const dispatch = useDispatch();
-
-  const onChangeUsername = (e) => {
-    const username = e.target.value;
-    setUsername(username);
-  };
-
-  const onChangeEmail = (e) => {
-    const email = e.target.value;
-    setEmail(email);
-  };
-
-  const onChangePassword = (e) => {
-    const password = e.target.value;
-    setPassword(password);
-  };
-
-  const handleRegister = (e) => {
-    e.preventDefault();
-
-    setSuccessful(false);
-
-    form.current.validateAll();
-
-    if (checkBtn.current.context._errors.length === 0) {
-      dispatch(register(username, email, password))
-        .then(() => {
-          setSuccessful(true);
-        })
-        .catch(() => {
-          setSuccessful(false);
-        });
+    const onSubmit=(data)=> {
+        // display form data on success
+        alert('SUCCESS!! :-)\n\n' + JSON.stringify(data, null, 4));
     }
-  };
 
-  return (
-    <div className="col-md-12">
-      <div className="card card-container">
-        {/* <img
-          src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
-          alt="profile-img"
-          className="profile-img-card"
-        /> */}
+    return (
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="form-group">
+                    <label>Full Name</label>
+                    <input
+                        name="fullname"
+                        type="text"
+                        {...register('fullname')}
+                        className={`form-control ${errors.fullname ? 'is-invalid' : ''}`}
+                    />
+                    <div className="invalid-feedback">{errors.fullname?.message}</div>
+                </div>
 
-        <Form onSubmit={handleRegister} ref={form}>
-          {!successful && (
-            <div>
-              <div className="form-group">
-                <label htmlFor="username">Username</label>
-                <Input
-                  type="text"
-                  className="form-control"
-                  name="username"
-                  value={username}
-                  onChange={onChangeUsername}
-                  validations={[required, vusername]}
-                />
-              </div>
+                <div className="form-group">
+                    <label>Username</label>
+                    <input
+                        name="username"
+                        type="text"
+                        {...register('username')}
+                        className={`form-control ${errors.username ? 'is-invalid' : ''}`}
+                    />
+                    <div className="invalid-feedback">{errors.username?.message}</div>
+                </div>
 
-              <div className="form-group">
-                <label htmlFor="email">Email</label>
-                <Input
-                  type="text"
-                  className="form-control"
-                  name="email"
-                  value={email}
-                  onChange={onChangeEmail}
-                  validations={[required, validEmail]}
-                />
-              </div>
+                <div className="form-group">
+                    <label>Email</label>
+                    <input
+                        name="email"
+                        type="text"
+                        {...register('email')}
+                        className={`form-control ${errors.email ? 'is-invalid' : ''}`}
+                    />
+                    <div className="invalid-feedback">{errors.email?.message}</div>
+                </div>
 
-              <div className="form-group">
-                <label htmlFor="password">Password</label>
-                <Input
-                  type="password"
-                  className="form-control"
-                  name="password"
-                  value={password}
-                  onChange={onChangePassword}
-                  validations={[required, vpassword]}
-                />
-                <label htmlFor="password">Confirm Password</label>
-                <Input
-                  type="password"
-                  className="form-control"
-                  name="password"
-                  value={password}
-                  onChange={onChangePassword}
-                  validations={[required, vpassword]}
-                />
+                <div className="form-group">
+                    <label>Password</label>
+                    <input
+                        name="password"
+                        type="password"
+                        {...register('password')}
+                        className={`form-control ${errors.password ? 'is-invalid' : ''}`}
+                    />
+                    <div className="invalid-feedback">{errors.password?.message}</div>
+                </div>
 
-              </div>
+                <div className="form-group">
+                    <label>Confirm Password</label>
+                    <input
+                        name="confirmPassword"
+                        type="password"
+                        {...register('confirmPassword')}
+                        className={`form-control ${errors.confirmPassword ? 'is-invalid' : ''
+                            }`}
+                    />
+                    <div className="invalid-feedback">
+                        {errors.confirmPassword?.message}
+                    </div>
+                </div>
 
-              <div className="form-group">
-                <button className="btn btn-primary btn-block">Sign Up</button>
-              </div>
-            </div>
-          )}
+                <div className="form-group form-check">
+                    <input
+                        name="acceptTerms"
+                        type="checkbox"
+                        {...register('acceptTerms')}
+                        className={`form-check-input ${errors.acceptTerms ? 'is-invalid' : ''
+                            }`}
+                    />
+                    <label htmlFor="acceptTerms" className="form-check-label">
+                    Accept Terms & Conditions
+                    </label>
+                    <div className="invalid-feedback">{errors.acceptTerms?.message}</div>
+                </div>
 
-          {message && (
-            <div className="form-group">
-              <div className={successful ? "alert alert-success" : "alert alert-danger"} role="alert">
-                {message}
-              </div>
-            </div>
-          )}
-          <CheckButton style={{ display: "none" }} ref={checkBtn} />
-        </Form>
-      </div>
-    </div>
-  );
-};
-
-export default Register;
+                <div className="form-group">
+                    <button type="submit" className="btn btn-primary">
+                        Register
+                    </button>
+                    <button
+                        type="button"
+                        onClick={reset}
+                        className="btn btn-warning float-right"
+                    >
+                        Reset
+                    </button>
+                </div>
+                <p className="forgot-password text-right">
+                    Already registered <a href="/login">Login?</a>
+                </p>
+            </form>
+    )
+}
