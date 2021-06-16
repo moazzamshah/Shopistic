8a429b68b4416789ba734e3c1a9874b939b64dcb
Ahmad575-a <ahmad.alghetheth@gmail.com>
2021-06-16 23:16:55 +0200

    created a Review Schema

---
+++ server/models/Review.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ReviewSchema = new Schema({
+    itemId: {
+        type: Schema.Types.ObjectId,
+        ref: 'items'
+    },
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+    },
+    user: {
+        type: String,
+        required: true
+    },
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    comment: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('review', ReviewSchema);
