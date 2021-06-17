51d2e6eba71bf404192aa541cdf0768fb0acd092
ArslanRama <arslanrama84@gmail.com>
2021-06-17 08:21:34 +0200

    forms updated

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,44 +1,63 @@
-import React, { useState } from "react";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
+import React from "react";
+import { useForm } from "react-hook-form";
 import "../css/login.css";
 
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from 'yup';
+
 export const Login = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  // YUP validation
+  const validationSchema = Yup.object().shape({
+    email: Yup.string()
+      .required('Email is required')
+      .email('Email is invalid'),
+    password: Yup.string()
+      .required('Password is required')
+      .min(6, 'Password must be at least 6 characters')
+      .max(40, 'Password must not exceed 40 characters'),
+  });
 
-    function validateForm() {
-        return email.length > 0 && password.length > 0;
-    }
+  // functions to build form returned by useForm() hook
+  const { register, handleSubmit, reset, formState: { errors } } = useForm({
+    resolver: yupResolver(validationSchema)
+  });
+  const onSubmit = (data) => {
+    // display form data on success
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(data, null, 4));
+  }
+  return (
 
-    function handleSubmit(event) {
-        event.preventDefault();
-    }
+    <form onSubmit={handleSubmit(onSubmit)}>
+      {/* Email */}
+      <div className="form-group">
+        <label>Email</label>
+        <input
+          name="email"
+          type="text"
+          {...register('email')}
+          className={`form-control ${errors.email ? 'is-invalid' : ''}`}
+        />
+        <div className="invalid-feedback">{errors.email?.message}</div>
+      </div>
+      {/* Password */}
+      <div className="form-group">
+        <label>Password</label>
+        <input
+          name="password"
+          type="password"
+          {...register('password')}
+          className={`form-control ${errors.password ? 'is-invalid' : ''}`}
+        />
+        <div className="invalid-feedback">{errors.password?.message}</div>
+      </div>
+      {/* Button */}
+      <div className="form-group">
+        <button type="submit" className="btn btn-primary">
+          Login
+        </button>
+      </div>
+    </form>
+  );
+};
 
-    return (
-        <div className="Login">
-            <Form onSubmit={handleSubmit}>
-                <Form.Group size="lg" controlId="email">
-                    <Form.Label>Email</Form.Label>
-                    <Form.Control
-                        autoFocus
-                        type="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                </Form.Group>
-                <Form.Group size="lg" controlId="password">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control
-                        type="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                </Form.Group>
-                <Button block size="lg" type="submit" disabled={!validateForm()}>
-                    Login
-                </Button>
-            </Form>
-        </div>
-    );
-}
+
