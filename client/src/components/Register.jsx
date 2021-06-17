81fab34400ef2811f7d7a3b5ebf6ad08129c0d3f
ArslanRama <arslanrama84@gmail.com>
2021-06-17 08:34:19 +0200

    implemented style on forms

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as Yup from 'yup';
+import '../css/register.css'
 
 export const Register = () => {
     // YUP validation
