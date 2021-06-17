0f7af1ae1aff5e193f3370bf1c6adf55eec73042
Syed Moazzam Ali Shah <Moazzam.shah106@gmail.com>
2021-06-17 10:56:09 +0200

    added navbar and routing in app and also added a 404 page not found component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,28 @@
-import React from 'react'
-
+import React from 'react';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Pricing from './components/Pricing';
+import About from './components/About';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import NoPageFound from './components/NoPageFound';
 const App = () => {
   return (
-    <div>
-      Final Project
-    </div>
-  )
-}
+    <Router>
+        <Navbar />
+      <div className='container mt-3'>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/pricing' component={Pricing} />
+          <Route path='/about' component={About} />
+          <Route path='/login' component={Login} />
+          <Route exact path='/signup' component={Signup} />
+          <Route component={NoPageFound} />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
 
-export default App
+export default App;
