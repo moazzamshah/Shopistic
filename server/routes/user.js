036e3173e07c426ce1e29d33f1f9fc8cdc4533f7
Ahmad575-a <ahmad.alghetheth@gmail.com>
2021-06-18 14:28:14 +0200

    created login route

--- server/routes/user.js
+++ server/routes/user.js
@@ -4,6 +4,7 @@ const User = require('../models/User')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const validateRegisteration = require('../validation/register')
+const validateLogin = require('../validation/login')
 
 router.post("/register", (req, res) => {
     const { errors, isValid } = validateRegisteration(req.body);
@@ -45,6 +46,40 @@ router.post("/register", (req, res) => {
         }
     });
 });
+
+router.post("/login", (req, res) => {
+    const { errors, isValid } = validateLogin(req.body);
+
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
+    const { email, password } = req.body
+
+    User.findOne({ email }).then(user => {
+        if (!user) {
+            errors.email = "User does not exist !!";
+            return res.status(400).json(errors);
+        }
+
+        bcrypt.compare(password, user.password).then(isMatch => {
+            if (isMatch) {
+                const payload = { id: user.id, email: user.email, name: user.name };
+
+                jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: 3600 }, (err, token) => {
+                    res.json({
+                        success: true,
+                        token: "Bearer " + token
+                    });
+                });
+            } else {
+                errors.password = "Incorrect password";
+                return res.status(400).json(errors);
+            }
+        });
+    });
+});
+
 
 
 module.exports = router
