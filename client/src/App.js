1d67910491dbf370cb33eb986883b5e64cd0ce56
ArslanRama <arslanrama84@gmail.com>
2021-06-21 09:41:23 +0200

    accepted changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
-
-import React from 'react'
-import { Register } from './components/Register'
-import  {Login}  from './components/Login'
-
-export default function App() {
-  return (
-    <div className='App'>
-    <h1 className='text-center'>Register Form</h1>
-      <Register />
-
-      <div className='container'>
-        <h1 className='text-center'>Login Form</h1>
-        <Login />
-      </div>
-      
-    </div>
-  )
-}
-=======
 import React from 'react';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
@@ -49,4 +26,3 @@ const App = () => {
 };
 
 export default App;
->>>>>>> origin/syed
