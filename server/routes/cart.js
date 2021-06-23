28745f1064da1978b815316645b8a8d5b73b7299
Ahmad575-a <ahmad.alghetheth@gmail.com>
2021-06-23 12:11:09 +0200

    get the saved products in the cart

--- server/routes/cart.js
+++ server/routes/cart.js
@@ -3,19 +3,28 @@ const router = express.Router()
 const passport = require('passport')
 const CartItem = require('../models/Cart_Item')
 
-// Add to the cart
-router.post('/',
-    passport.authenticate('jwt', { session: false }),
-    (req, res) => {
-        const newCartItem = new CartItem({
-            userId: req.body.userId,
-            itemId: req.body.itemId
-        });
+// Add the products to the cart
+router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+    const newCartItem = new CartItem({
+        userId: req.body.userId,
+        itemId: req.body.itemId
+    });
 
-        newCartItem
-            .save()
-            .then(cartItem => res.json(cartItem));
-    })
+    newCartItem
+        .save()
+        .then(cartItem => res.json(cartItem));
+})
+
+
+// get the saved products in the cart
+router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+    CartItem.find({ userId: req.user.id })
+        .populate("itemId")
+        .then(cartItems => res.json(cartItems))
+        .catch(err =>
+            res.status(404).json({ noCartItemFound: 'No Items found from this user' })
+        );
+});
 
 
 module.exports = router;
