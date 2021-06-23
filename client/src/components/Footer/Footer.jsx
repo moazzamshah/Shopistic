944bf9023d60561002e13767dd79ec853370fe0c
ArslanRama <arslanrama84@gmail.com>
2021-06-23 12:30:58 +0200

    Bug fixed

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -4,22 +4,21 @@ import './footer.css'
 export default function Footer() {
   return (
    <div>
-    <div class="container"></div>
+    <div className="container"></div>
     {/* Footer main */}
     <footer>
-      <section class="ft-main">
-        <div class="ft-main-item">
-          <h2 class="ft-title">About</h2>
+      <section className="ft-main">
+        <div className="ft-main-item">
+          <h2 className="ft-title">About</h2>
           <ul>
             <li><a href="#">Services</a></li>
-
             <li><a href="#">Pricing</a></li>
             <li><a href="#">Customers</a></li>
             <li><a href="#">Careers</a></li>
           </ul>
         </div>
-        <div class="ft-main-item">
-          <h2 class="ft-title">Resources</h2>
+        <div className="ft-main-item">
+          <h2 className="ft-title">Resources</h2>
           <ul>
             <li><a href="#">Docs</a></li>
             <li><a href="#">Blog</a></li>
@@ -27,16 +26,16 @@ export default function Footer() {
             <li><a href="#">Webinars</a></li>
           </ul>
         </div>
-        <div class="ft-main-item">
-          <h2 class="ft-title">Contact</h2>
+        <div className="ft-main-item">
+          <h2 className="ft-title">Contact</h2>
           <ul>
             <li><a href="#">Help</a></li>
             <li><a href="#">Sales</a></li>
             <li><a href="#">Advertise</a></li>
           </ul>
         </div>
-        <div class="ft-main-item">
-          <h2 class="ft-title">Stay Updated</h2>
+        <div className="ft-main-item">
+          <h2 className="ft-title">Stay Updated</h2>
           <p>Subscribe to our newsletter to get our latest news.</p>
           <form>
             <input type="email" name="email" placeholder="Enter email address"/>
@@ -45,20 +44,20 @@ export default function Footer() {
         </div>
       </section>
       {/* Footer social */}
-      <section class="ft-social">
-        <ul class="ft-social-list">
-          <li><a href="#"><i class="fab fa-facebook"></i></a></li>
-          <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-          <li><a href="#"><i class="fab fa-instagram"></i></a></li>
-          <li><a href="#"><i class="fab fa-github"></i></a></li>
-          <li><a href="#"><i class="fab fa-linkedin"></i></a></li>
-          <li><a href="#"><i class="fab fa-youtube"></i></a></li>
+      <section className="ft-social">
+        <ul className="ft-social-list">
+          <li><a href="#"><i className="fab fa-facebook"></i></a></li>
+          <li><a href="#"><i className="fab fa-twitter"></i></a></li>
+          <li><a href="#"><i className="fab fa-instagram"></i></a></li>
+          <li><a href="#"><i className="fab fa-github"></i></a></li>
+          <li><a href="#"><i className="fab fa-linkedin"></i></a></li>
+          <li><a href="#"><i className="fab fa-youtube"></i></a></li>
         </ul>
       </section>
      
      {/* Footer legal  */}
-      <section class="ft-legal">
-        <ul class="ft-legal-list">
+      <section className="ft-legal">
+        <ul className="ft-legal-list">
           <li><a href="#">Terms &amp; Conditions</a></li>
           <li><a href="#">Privacy Policy</a></li>
           <li>&copy; 2021 Copyright ASA.</li>
