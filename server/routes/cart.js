dcf5ed655f5db992a1274480bc01e5c95d6a8ef5
Ahmad575-a <ahmad.alghetheth@gmail.com>
2021-06-24 22:51:02 +0200

    delete a product from the cart

--- server/routes/cart.js
+++ server/routes/cart.js
@@ -26,5 +26,15 @@ router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => 
         );
 });
 
+// delete a product from the cart
+router.delete("/:cart_item_id", passport.authenticate('jwt', { session: false }), (req, res) => {
+    CartItem.findByIdAndRemove(req.params.cart_item_id, err => {
+        if (err) res.send(err);
+        else res.json({
+            message: "the item has been deleted from cart"
+        });
+    });
+});
+
 
 module.exports = router;
