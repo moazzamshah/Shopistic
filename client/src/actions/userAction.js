9834908afdf25f45dd4eb4b9333ae7f579bae94d
Ahmad575-a <ahmad.alghetheth@gmail.com>
2021-07-08 15:11:30 +0200

    created a userAction to reset the password

--- client/src/actions/userAction.js
+++ client/src/actions/userAction.js
@@ -1,24 +1,24 @@
 import axios from 'axios';
-import { USER_DETAILS_FAIL, USER_DETAILS_REQUEST, USER_DETAILS_SUCCESS, USER_REGISTER_FAIL, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS, USER_SIGNIN_FAIL, USER_SIGNIN_REQUEST, USER_SIGNIN_SUCCESS, USER_SIGNOUT, USER_UPDATE_PROFILE_FAIL, USER_UPDATE_PROFILE_REQUEST, USER_UPDATE_PROFILE_SUCCESS } from '../constants/userConstants';
+import { USER_DETAILS_FAIL, USER_DETAILS_REQUEST, USER_DETAILS_SUCCESS, USER_REGISTER_FAIL, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS, USER_SIGNIN_FAIL, USER_SIGNIN_REQUEST, USER_SIGNIN_SUCCESS, USER_SIGNOUT, USER_UPDATE_PROFILE_FAIL, USER_UPDATE_PROFILE_REQUEST, USER_UPDATE_PROFILE_SUCCESS, RESET_USER } from '../constants/userConstants';
 
 
 //register action 
 export const register = (name, email, password) => async (dispatch) => {
-    dispatch({ type: USER_REGISTER_REQUEST, payload: { name, email, password}});
+    dispatch({ type: USER_REGISTER_REQUEST, payload: { name, email, password } });
 
-    try{
+    try {
         // use axios for http post request when user REGISTERg in 
-        const { data } = await axios.post('/api/users/register', { name, email, password});
+        const { data } = await axios.post('http://localhost:8000/api/user/register', { name, email, password });
         // if success, dispatch success and set payload to data 
-        dispatch({ type: USER_REGISTER_SUCCESS, payload:  data });
+        dispatch({ type: USER_REGISTER_SUCCESS, payload: data });
         //also dispatch SignIn_Success because userSignin.userInfo is what we use to valid user
-        dispatch({ type: USER_SIGNIN_SUCCESS, payload:  data });
+        dispatch({ type: USER_SIGNIN_SUCCESS, payload: data });
         // save data to localStorage
         localStorage.setItem('userInfo', JSON.stringify(data));
 
-    }catch(error) {
+    } catch (error) {
         // if error, dispatch FAIL, set payload to error message 
-        dispatch({ type: USER_REGISTER_FAIL, payload: error.response && error.response.data.message ? error.response.data.message : error.message});
+        dispatch({ type: USER_REGISTER_FAIL, payload: error.response && error.response.data.message ? error.response.data.message : error.message });
     }
 };
 
@@ -26,19 +26,19 @@ export const register = (name, email, password) => async (dispatch) => {
 
 // signin action 
 export const signin = (email, password) => async (dispatch) => {
-    dispatch({ type: USER_SIGNIN_REQUEST, payload: { email, password}});
+    dispatch({ type: USER_SIGNIN_REQUEST, payload: { email, password } });
 
-    try{
+    try {
         // use axios for http post request when user signing in 
-        const { data } = await axios.post('/api/users/signin', { email, password});
+        const { data } = await axios.post('http://localhost:8000/api/user/login', { email, password });
         // if success, dispatch success and set payload to data 
-        dispatch({ type: USER_SIGNIN_SUCCESS, payload:  data });
+        dispatch({ type: USER_SIGNIN_SUCCESS, payload: data });
         // save data to localStorage
         localStorage.setItem('userInfo', JSON.stringify(data));
 
-    }catch(error) {
+    } catch (error) {
         // if error, dispatch FAIL, set payload to error message 
-        dispatch({ type: USER_SIGNIN_FAIL, payload: error.response && error.response.data.message ? error.response.data.message : error.message});
+        dispatch({ type: USER_SIGNIN_FAIL, payload: error.response && error.response.data.message ? error.response.data.message : error.message });
     }
 };
 
@@ -47,7 +47,7 @@ export const signout = () => (dispatch) => {
     //remove userInfo and cartItems from localStorage upon user signout 
     localStorage.removeItem('userInfo');
     localStorage.removeItem('cartItems');
-    dispatch({ type: USER_SIGNOUT});
+    dispatch({ type: USER_SIGNOUT });
 };
 
 //user profile detail action 
@@ -56,7 +56,7 @@ export const detailsUser = (userId) => async (dispatch, getState) => {
     //get userInfo from store
     const { userSignin: { userInfo } } = getState();
 
-    try{
+    try {
         //send ajax request 
         const { data } = await axios.get(`/api/users/${userId}`, {
             headers: {
@@ -64,10 +64,10 @@ export const detailsUser = (userId) => async (dispatch, getState) => {
             }
         });
 
-        dispatch({type: USER_DETAILS_SUCCESS, payload: data});
-    } catch(error) {
+        dispatch({ type: USER_DETAILS_SUCCESS, payload: data });
+    } catch (error) {
         // if error, dispatch FAIL, set payload to error message 
-        dispatch({ type: USER_DETAILS_FAIL, payload: error.response && error.response.data.message ? error.response.data.message : error.message});
+        dispatch({ type: USER_DETAILS_FAIL, payload: error.response && error.response.data.message ? error.response.data.message : error.message });
     }
 };
 
@@ -77,7 +77,7 @@ export const updateUserProfile = (user) => async (dispatch, getState) => {
     //get userInfo from store
     const { userSignin: { userInfo } } = getState();
 
-    try{
+    try {
         //send ajax request 
         const { data } = await axios.put('/api/users/profile', user, {
             headers: {
@@ -85,13 +85,24 @@ export const updateUserProfile = (user) => async (dispatch, getState) => {
             }
         });
 
-        dispatch({type: USER_UPDATE_PROFILE_SUCCESS, payload: data});
+        dispatch({ type: USER_UPDATE_PROFILE_SUCCESS, payload: data });
         //update userSignin as well to update the display
-        dispatch({type:USER_SIGNIN_SUCCESS, payload: data});
+        dispatch({ type: USER_SIGNIN_SUCCESS, payload: data });
         //update localStorage after profile updated
         localStorage.setItem('userInfo', JSON.stringify(data));
-    } catch(error) {
+    } catch (error) {
         // if error, dispatch FAIL, set payload to error message 
-        dispatch({ type: USER_UPDATE_PROFILE_FAIL, payload: error.response && error.response.data.message ? error.response.data.message : error.message});
+        dispatch({ type: USER_UPDATE_PROFILE_FAIL, payload: error.response && error.response.data.message ? error.response.data.message : error.message });
     }
+};
+
+export const attemptResetPassword = (password, id) => async (dispatch) => {
+    dispatch({ type: RESET_USER, payload: { password, id } });
+    try {
+        const resetPassword = await axios.post(`http://localhost:8000/api/user/resetPassword/password/${id}`)
+        await resetPassword(password, id)
+    } catch(error) {
+        dispatch(error.response.data.message)
+    }
+
 };
