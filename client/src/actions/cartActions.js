4ff6059ab1d6ee992ca48bcf16eebb4414fe16d2
Ahmad575-a <ahmad.alghetheth@gmail.com>
2021-07-27 17:46:09 +0200

    added cart actions

--- client/src/actions/cartActions.js
+++ client/src/actions/cartActions.js
@@ -1,42 +1,55 @@
 import axios from 'axios';
 import { CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SAVE_PAYMENT_METHOD, CART_SAVE_SHIPPING_ADDRESS } from '../constants/cartConstants';
 
+export const RECEIVE_CART_ITEMS = "RECEIVE_CART_ITEMS";
+
+const receiveCartItems = (cartItems) => ({
+	type: RECEIVE_CART_ITEMS,
+	cartItems
+});
+
+export const fetchCartItems = (userId) => (dispatch, getState) => {
+    const { userSignin: { userInfo } } = getState()
+    axios.get('http://localhost:8000/api/cart', {userId}, {
+        headers: {
+          Authorization: `${userInfo?.token}`
+        }
+      })
+    .then(cartItems => dispatch(receiveCartItems(cartItems)))
+}
 // when we define an action function, it should return an async function with dispatch
 // disptach and getState are redux-thunk functions to get access to redux store
-export const addToCart = (productId, qty) => async (dispatch, getState) => {
+export const addToCart = (productId, userId, qty) => async (dispatch, getState) => {
+    const { userSignin: { userInfo } } = getState()
     // data (product) is deconstructed from axios return data
-    const { data } = await axios.get(`/api/products/${productId}`);
-
+    const { data } = await axios.post(`http://localhost:8000/api/cart`, {productId, userId, qty},{
+        headers: {
+          Authorization: `${userInfo?.token}`
+        }
+      });
+      console.log(data, "data")
     //using product data to dispatch
     dispatch({
         type: CART_ADD_ITEM,
         payload: {
-            name: data.name,
-            image: data.image,
-            price: data.price,
-            countInStock: data.countInStock,
-            product: data._id,
+            productId,
+            userId,
             qty
         }
     });
 
-    //after adding items to cart, get cartItems from redux store using getState and store data in localStorage
-    localStorage.setItem(
-        'cartItems',
-        JSON.stringify(getState().cart.cartItems)
-    );
 };
 
 //remove item from cart action
 export const removeFromCart = (productId) => (dispatch, getState) => {
-    dispatch({type: CART_REMOVE_ITEM, payload: productId});
+    dispatch({ type: CART_REMOVE_ITEM, payload: productId });
 
     localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
 };
 
 // saveShipping address
 export const saveShippingAddress = (data) => (dispatch) => {
-    dispatch({type: CART_SAVE_SHIPPING_ADDRESS, payload: data});
+    dispatch({ type: CART_SAVE_SHIPPING_ADDRESS, payload: data });
     //save shipping address to localStorage
     localStorage.setItem('shippingAddress', JSON.stringify(data));
 };
@@ -44,5 +57,5 @@ export const saveShippingAddress = (data) => (dispatch) => {
 
 // savePaymentMethod action
 export const savePaymentMethod = (data) => (dispatch) => {
-    dispatch({type: CART_SAVE_PAYMENT_METHOD, payload: data});
+    dispatch({ type: CART_SAVE_PAYMENT_METHOD, payload: data });
 };
