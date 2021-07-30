e3f72e8c1d4f9d82ba40568ba4229fba8601f6d4
Ahmad575-a <ahmad.alghetheth@gmail.com>
2021-07-30 13:26:19 +0200

    limiting the requests

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,14 +2,22 @@ const express = require("express")
 const router = express.Router()
 const passport = require('passport')
 const UserController = require('../Controllers/userController')
+const limitter = require('express-rate-limit')
 
+const loginLimitter = limitter({
+    windowMs: 60 * 1000 ,
+    max: 1000,
+    message:{
+        code:429,
+        message: 'Too many request !! try later'
+    }
+})
 router.post("/register", UserController.register);
-router.post("/login", UserController.Login);
-router.post('/forgetpassword', UserController.ForgetPassword)
+router.post("/login", loginLimitter, UserController.Login);
 
+router.post('/forgetpassword', UserController.ForgetPassword)
 // forget password : compare the token
 router.get('/resetPassword/:token', UserController.CompareToken)
-
 // change the password in the database
 router.post('/resetPassword/password/:id', UserController.ResetPassword)
 
